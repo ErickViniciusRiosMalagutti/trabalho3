95e1cf04ba29beee2efbfcfa2f5220d26ef10a3b
ErickViniciusRiosMalagutti <165736646+ErickViniciusRiosMalagutti@users.noreply.github.com>
2024-06-09 21:43:42 -0300

    Update atividadE7.js

--- floppa 4.0/atividadE7.js
+++ floppa 4.0/atividadE7.js
@@ -4,5 +4,17 @@ let btt = document.querySelector("#btt");
 let resposta = document.querySelector("#resposta");
 
 function calcular(){
-    
+    let dia1 = number(dia.value);
+    let mes1 = number(mes.value);
+    if (mes1 !=12){
+        let resp =(((MES1 - 1)* 30) + dia1);
+        resposta.textContent = resp;
+}else{
+        let resp = ((11 * 30) + dia1 + 4);
+        resposta.textContent = resp;
+    }
+
+}
+btt.onclick = function(){
+    calcular()
 }
